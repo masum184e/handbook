1ebffb1f5e7a6441f55582daa648a962d4a2dd28
masum184e <masum184e@gmail.com>
2026-01-18 13:22:59 +0600

    feat: add system-design

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,6 +1,8 @@
 import { themes as prismThemes } from 'prism-react-renderer';
 import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
@@ -44,6 +46,8 @@ const config: Config = {
           path: 'docs',
           routeBasePath: 'docs',
           sidebarPath: './sidebars.ts',
+              remarkPlugins: [remarkMath],
+        rehypePlugins: [rehypeKatex],
         },
         // blog: {
         //   showReadingTime: true,
